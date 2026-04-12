b597a8bd5075f5496eda7ef6ffc896d9ec7fb130
Martin Hrubes <Martin.Hrubes@ys.cz>
2026-04-12 18:00:55 +0200

    uprava nahledu trate na uvodni strance

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -6,7 +6,7 @@ export const TRACKS: TrackDef[] = [
     id: "monza",
     nameKey: "tracks.monza",
     lapTimeSeconds: 79,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/monza.svg",
     /** Jednoduchá „dokola“ – protáhlá smyčka (rýchlostní charakter) */
     pathD:
@@ -18,7 +18,7 @@ export const TRACKS: TrackDef[] = [
     id: "spa",
     nameKey: "tracks.spa",
     lapTimeSeconds: 106,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/spa.svg",
     pathD:
       "M 70 420 L 95 280 L 210 220 L 340 260 L 415 180 L 545 145 L 675 175 L 780 135 L 910 210 L 925 350 L 845 470 L 715 520 L 545 495 L 400 530 L 255 495 L 145 505 L 75 460 L 70 420 Z",
@@ -29,7 +29,7 @@ export const TRACKS: TrackDef[] = [
     id: "silverstone",
     nameKey: "tracks.silverstone",
     lapTimeSeconds: 88,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/silverstone.svg",
     pathD:
       "M 120 140 L 290 110 L 455 150 L 615 125 L 765 185 L 835 305 L 795 445 L 655 535 L 485 515 L 330 555 L 185 490 L 125 320 L 120 140 Z",
@@ -40,7 +40,7 @@ export const TRACKS: TrackDef[] = [
     id: "monaco",
     nameKey: "tracks.monaco",
     lapTimeSeconds: 74,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/monaco.svg",
     pathD:
       "M 155 500 L 155 215 L 285 140 L 455 160 L 595 135 L 745 195 L 815 315 L 775 445 L 645 525 L 495 505 L 355 530 L 220 505 L 155 500 Z",
@@ -51,7 +51,7 @@ export const TRACKS: TrackDef[] = [
     id: "suzuka",
     nameKey: "tracks.suzuka",
     lapTimeSeconds: 92,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/suzuka.svg",
     pathD:
       "M 485 95 C 665 75 840 190 825 330 C 805 435 700 485 595 470 C 505 458 455 520 355 515 C 215 505 105 395 125 265 C 145 165 265 95 400 95 C 430 95 460 95 485 95 Z",
@@ -62,7 +62,7 @@ export const TRACKS: TrackDef[] = [
     id: "interlagos",
     nameKey: "tracks.interlagos",
     lapTimeSeconds: 72,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/interlagos.svg",
     /** Druhá jednoduchá „dokola“ – kompaktnější ovál */
     pathD:
@@ -74,7 +74,7 @@ export const TRACKS: TrackDef[] = [
     id: "ridgeway",
     nameKey: "tracks.ridgeway",
     lapTimeSeconds: 94,
-    previewSpeedFactor: 2,
+    previewSpeedFactor: 10,
     imageSrc: "/generated/tracks/ridgeway.svg",
     /** Dlouhá spodní rovinka, ostrá levá, šikmý úsek, dva „vrcholy“ + V, technická pravá */
     pathD:
